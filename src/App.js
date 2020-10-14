2271249398132f74fcaa2e42f5e3427bc5f61c5f
UsernameQQQ <carry789@naver.com>
2020-10-14 17:19:04 +0900

    temp commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const foodILike = [
+  {
+    name: "Kimchi",
+    image:
+      "https://www.maangchi.com/wp-content/uploads/2017/03/kimchi-insta.jpg",
+  },
+  {
+    name: "Samgyeopsal",
+    image:
+      "https://www.gildedgingerbread.com/wp-content/uploads/2017/08/Samgyeopsal-1.jpg",
+  },
+  {
+    name: "Bibimbap",
+    image:
+      "http://foodrecipesearch.com/wp-content/uploads/2017/09/04-30-minute-korean-bibimbap.jpg",
+  },
+  {
+    name: "Donkatsu",
+    image:
+      "https://s3-media4.fl.yelpcdn.com/bphoto/anesu6IAyAj2d6r19eFqEg/o.jpg",
+  },
+  {
+    name: "Kimbap",
+    image: "https://swisshansik.files.wordpress.com/2011/09/kimbap-2.jpg",
+  },
+];
+
+function Food({ name, pic }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>like {name}</h1>
+      <img src={pic}></img>
     </div>
   );
+}
+
+function renderFood(dish) {
+  return <Food name={dish.name} pic={dish.image} />;
+}
+
+function App() {
+  return <div>{foodILike.map(renderFood)}</div>;
 }
 
 export default App;
